0882fb2e366c18574830b40391becac6805499e2
Dano Sato <uchang01@gmail.com>
2025-12-23 08:07:42 +0900

    251223

---
+++ 251223.py
@@ -0,0 +1,116 @@
+from REMOLib import *
+
+
+
+
+
+
+#게임 오브젝트들을 선언하는 곳입니다.
+class Obj:
+    None
+
+class myCard(rectObj):
+    def __init__(self,rect,color=Cs.white,name="",**kwargs):
+        super().__init__(rect,color=color,**kwargs)
+        self.nameText = textObj(name,size=24,color=Cs.black)
+        self.nameText.setParent(self)
+        self.nameText.center = self.offsetRect.midtop + RPoint(0,30)
+    def draw(self):
+        super().draw()
+        return
+
+class myCheckbox(rectObj):
+    color_picked = Cs.salmon
+    color_unpicked = Cs.gray
+
+    def __init__(self,rect,checked=True,**kwargs):
+        if checked:
+            color = self.color_picked
+        else:
+            color = self.color_unpicked
+        super().__init__(rect,color=color,**kwargs)
+        self.checked = checked
+        self.inner_checkmark = imageObj(Icons.CHECKMARK)
+        scale = min(self.offsetRect.w,self.offsetRect.h)*0.9 / self.inner_checkmark.offsetRect.w
+        self.inner_checkmark.scale = scale
+
+        self.inner_checkmark.setParent(self)
+        self.inner_checkmark.center = self.offsetRect.center
+    def toggle(self):
+        self.checked = not self.checked
+        if self.checked:
+            self.inner_checkmark.alpha = 255
+            self.color = self.color_picked
+        else:
+            self.inner_checkmark.alpha = 0
+            self.color = self.color_unpicked
+    def draw(self):
+        super().draw()
+        return
+
+class mainScene(Scene):
+    def initOnce(self):
+        self.cards: list[myCard] = []
+        self.checkboxes: list[myCheckbox] = []
+        for i in range(5):
+            card = myCard(pygame.Rect(0,0,200,300),color=Cs.yellow,name=f"My Card {i+1}")
+            self.cards.append(card)
+        self.cardLayout = layoutObj(childs=self.cards,isVertical=False)
+        self.cardLayout.center = Rs.screenRect().center + RPoint(0,-100)
+        for i in range(5):
+            checkbox = myCheckbox(pygame.Rect(0,0,100,100))
+            checkbox.setParent(self.cardLayout[i])
+            checkbox.midtop = self.cardLayout[i].offsetRect.midbottom + RPoint(0,20)
+            self.checkboxes.append(checkbox)
+
+        self.select_two_button = textButton("select Two")
+        self.select_two_button.center = Rs.screenRect().center + RPoint(0,250)
+        def check_select_two():
+            l = [cb.checked for cb in self.checkboxes if cb.checked]
+            if len(l)==2:
+                print("approved")
+            else:
+                print("nope")
+
+        self.select_two_button.connect(check_select_two)
+        return
+    def init(self):
+        return
+    def update(self):
+
+        self.select_two_button.update()
+        if Rs.userJustLeftClicked():
+            for checkbox in self.checkboxes:
+                if checkbox.collideMouse():
+                    checkbox.toggle()
+                    print(f"Checked status: {[cb.checked for cb in self.checkboxes]}")
+    def draw(self):
+        for card in self.cards:
+            card.draw()
+        for checkbox in self.checkboxes:
+            checkbox.draw()
+        self.select_two_button.draw()
+        return
+
+
+class defaultScene(Scene):
+    def initOnce(self):
+        return
+    def init(self):
+        return
+    def update(self):
+        return
+    def draw(self):
+        return
+
+class Scenes:
+    mainScene = mainScene()
+
+
+if __name__=="__main__":
+    #Screen Setting
+    window = REMOGame(window_resolution=(1920,1080),screen_size=(2560,1440),fullscreen=False,caption="DEFAULT")
+    window.setCurrentScene(Scenes.mainScene)
+    window.run()
+
+    # Done! Time to quit.
