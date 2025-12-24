c48b16572e2f23d4cc499c2100a8a0afdba0e3a2
Dano Sato <uchang01@gmail.com>
2025-12-24 13:21:35 +0900

    .

--- 251224.py
+++ 251224.py
@@ -2,8 +2,27 @@ from REMOLib import *
 
 
 
+@dataclass
+class cardData:
+    name: str
+    description: str
+    effect: str
 
+    def clone(self):
+        return replace(self)
 
+CARD_LIBRARY = [
+    cardData(name="Card 1", description="Score + 1", effect = "score1"),
+    cardData(name="Card 2", description="Score + 10", effect = "score10"),
+    cardData(name="Card 3", description="Score + 100", effect = "score100")
+
+]
+
+class cardWidget(rectObj):
+    HEIGHT = 350
+    WIDTH = 210
+    def __init__(self,card:cardData):
+        super().__init__(pygame.Rect(0,0,self.WIDTH,self.HEIGHT))
 
 #게임 오브젝트들을 선언하는 곳입니다.
 class Obj:
@@ -13,6 +32,8 @@ class mainScene(Scene):
     def initOnce(self):
         self.a = textObj("New game")
         self.a.midtop = Rs.screenRect().midtop + RPoint(0,30)
+
+        self.hand = cardLayout(pos=RPoint(100,700))
         return
     def init(self):
         return
